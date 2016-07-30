ffce89c2fe109706a930c90bc527af8f343994ba
4lxp <#V45f0S6rfa&>
2016-07-30 22:25:52 +0200

    Added detailImageView toggle, to show/hide navigation UI on click

--- app/src/main/java/com/example/alex/abstruct/DetailActivity.java
+++ app/src/main/java/com/example/alex/abstruct/DetailActivity.java
@@ -3,8 +3,8 @@ package com.example.alex.abstruct;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.FrameLayout;
@@ -17,6 +17,11 @@ import uk.co.senab.photoview.PhotoViewAttacher;
 
 
 public class DetailActivity extends AppCompatActivity {
+
+    boolean isFirstTouch=true;
+    FrameLayout frameLayout;
+    int dominantColor;
+    LinearLayout authorLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +44,7 @@ public class DetailActivity extends AppCompatActivity {
 
     }
 
+
     //If thh arrow on toolbar is clicked, call the same method of the back button
     @Override
     public boolean onOptionsItemSelected(MenuItem menuItem) {
@@ -55,24 +61,15 @@ public class DetailActivity extends AppCompatActivity {
         ImageView detailImageView = (ImageView) findViewById(R.id.detailImageView);
         CircularImageView authorImageView = (CircularImageView) findViewById(R.id.authorImageView);
         TextView authorNameTextView = (TextView) findViewById(R.id.authorNameTextView);
-        FrameLayout frameLayout = (FrameLayout) findViewById(R.id.frameLayout);
-        LinearLayout authorLayout = (LinearLayout) findViewById(R.id.authorLayout);
+        frameLayout = (FrameLayout) findViewById(R.id.frameLayout);
+        authorLayout = (LinearLayout) findViewById(R.id.authorLayout);
 
 
         //Get the onject from the intent
         ImageClass imageObject = (ImageClass) getIntent().getSerializableExtra("imageObject");
 
-        //Set data
-        Picasso.with(this).load(imageObject.getImageRegularUrl()).into(detailImageView);
-        Picasso.with(this).load(imageObject.getAuthorImage()).into(authorImageView);
-        authorNameTextView.setText(imageObject.getAuthorName());
-
-        //Add pan and zoom funcionality to detailImageView
-        PhotoViewAttacher attacher = new PhotoViewAttacher(detailImageView);
-
-
         //Get and  edit colors
-        int dominantColor =Color.parseColor(imageObject.getColor());
+        dominantColor =Color.parseColor(imageObject.getColor());
 
         //Code to make a color darker or brighter
         float[] hsv;
@@ -91,8 +88,8 @@ public class DetailActivity extends AppCompatActivity {
 
         //Set color to various elements
         Window window = getWindow();
-            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-            window.setStatusBarColor(darkDominantColor);
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.setStatusBarColor(darkDominantColor);
 
         window.setNavigationBarColor(darkDominantColor);
 
@@ -101,6 +98,42 @@ public class DetailActivity extends AppCompatActivity {
         authorLayout.setBackgroundColor(darkDominantColor);
 
         authorNameTextView.setTextColor(brightDominantColor);
+
+
+        //Set data
+        Picasso.with(this).load(imageObject.getImageRegularUrl()).into(detailImageView);
+        Picasso.with(this).load(imageObject.getAuthorImage()).into(authorImageView);
+        authorNameTextView.setText(imageObject.getAuthorName());
+
+        //Add pan and zoom funcionality to detailImageView
+        PhotoViewAttacher attacher = new PhotoViewAttacher(detailImageView);
+        //Intercept Click on image view to hide and show navigation
+        attacher.setOnViewTapListener(new PhotoViewAttacher.OnViewTapListener() {
+            @Override
+            public void onViewTap(View view, float x, float y) {
+                if(isFirstTouch) {
+                    isFirstTouch=false;
+
+                    getWindow().getDecorView().setSystemUiVisibility(
+                            View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                                    | View.SYSTEM_UI_FLAG_FULLSCREEN
+                                    | View.SYSTEM_UI_FLAG_IMMERSIVE);
+
+                    frameLayout.setBackgroundColor(Color.BLACK);
+                    authorLayout.setVisibility(View.INVISIBLE);
+
+                }else{
+                    isFirstTouch=true;
+
+                    getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_VISIBLE);
+                    frameLayout.setBackgroundColor(dominantColor);
+                    authorLayout.setVisibility(View.VISIBLE);
+                }
+            }
+        });
 
 
     }
